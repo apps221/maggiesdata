fb1bc98b5e351ebdbb139eaeae27cb509f2cadce
Maggie <maggie.a.hong@gmail.com>
2025-06-08 18:13:28 -0400

    changed phone number label

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -46,7 +46,7 @@ export default function ContactMe() {
             />
           </label>
           <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">phone-number</span>
+            <span className="text-md">Phone Number</span>
             <input
               type="number"
               className="contact--input text-md"
